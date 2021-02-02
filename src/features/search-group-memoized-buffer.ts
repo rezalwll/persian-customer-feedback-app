17ca82fd39311ba15cb8898f8670ae1704cc7682
Mohamadreza <243572919+rezalwll@users.noreply.github.com>
2021-02-02 10:19:00 +0330

    feat(search): group memoized buffer

---
+++ src/features/search-group-memoized-buffer.ts
@@ -0,0 +1,4 @@
+/** group the memoized buffer from a numeric series. */
+export function groupSearchMemoizedBuffer(values: readonly number[]): number {
+  return values.reduce((total, value) => total + (value % 2 === 0 ? 1 : 0), 0);
+}
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { indexInventoryMemoizedBuffer } from './features/inventory-index-memoize
 export { deriveNavigationMemoizedBuffer } from './features/navigation-derive-memoized-buffer';
 export { normalizeOrdersMemoizedBuffer } from './features/orders-normalize-memoized-buffer';
 export { selectPricingMemoizedBuffer } from './features/pricing-select-memoized-buffer';
+export { groupSearchMemoizedBuffer } from './features/search-group-memoized-buffer';
